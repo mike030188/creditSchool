445f797ff14302e1966a1243b92f3795ac14ebda
mike030188 <halol.imnida2023@gmail.com>
2023-07-02 12:32:13 +0500

    feat: Wrapper1 with features has been implemented

--- src/components/navbar/navBar.jsx
+++ src/components/navbar/navBar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Logo, Navbar, Menu, Button, Body, Container1, Wrapper1 } from '../styled';
+import { Logo, Navbar, Menu, Button, Body, Container1, Wrapper1, Frame, Container2 } from '../styled';
 import logo from '../../assets/images/Logo.png';
+import smile from '../../assets/images/smile.png';
 
 
 
@@ -25,7 +26,22 @@ const NavBar = () => {
       </Container1>           
       </Navbar>
       <Wrapper1>
-        Hello
+        <h1>A virtual university,<br />
+          with real degrees</h1>
+        <p>Credischool is an accredited virtual university built<br />
+         by top professors from Harvard, Stanford, MIT, and<br />
+         many more. The students at Credischool are<br />
+         welcome to study at their own pace.</p>
+        <Button>Learn more</Button>
+        <Frame blue style={{marginLeft:"790px"}}>
+          <Container2 left>
+            <img src={smile} alt="smile icon"/>
+          </Container2>
+          <Container2 right>
+            <p>Total stipends given</p>
+            <h1>$10.508.305</h1>
+          </Container2>          
+        </Frame>
       </Wrapper1>
     </Body>
      
--- src/components/styled.js
+++ src/components/styled.js
@@ -2,12 +2,13 @@ import styled from 'styled-components';
 import backimg1 from '../assets/images/Photo1.png';
 import backimg2 from '../assets/images/Photo.png';
 
+
+
 export const Body = styled.div`
 background-color: #FCFCFF;
 background-image: url(${backimg2});
 background-repeat: no-repeat;
 background-position: right top;
-
 
 `
 
@@ -17,21 +18,24 @@ background-color:transparent;
 height: 104px;
 align-items: center;
 
-
 `
+
 export const Container1 = styled.div`
 display: flex;
 align-items: center;
-flex: ${props=>props.left || props.right ? "1" : "2" };
+flex: ${props=>props.left || props.right ? "1" : "1" };
 justify-content: ${props=>props.right ? "right" : "left"};
 
 `
+
+
 export const Logo = styled.img`
 height:40px;
 width: 203px;
 margin-left:320px;
 
 `
+
 export const Menu = styled.div`
 color: ${props=>props.black ? "#030F4B" : "#808396"};
 font-size: 18px;
@@ -41,8 +45,8 @@ margin:30px;
 
 `
 
-export const Button = styled.button`
 
+export const Button = styled.button`
 border-radius: 8px;
 border:none;
 font-size: 18px;
@@ -55,16 +59,64 @@ margin-right: ${props=>props.register ? "320px" : "0"};
 
 `
 
+
 export const Wrapper1 = styled.div`
 background-image: url(${backimg1});
 background-repeat: no-repeat;
 background-position: right bottom;
-
-
 height: 920px;
+padding-left: 320px;
+padding-top: 160px;
+
+h1{
+    color: var(--black-standard, #030F4B);
+    font-size: 80px;    
+    font-weight: 900;
+    line-height: normal;
+    margin-bottom: 40px;
+}
+p {
+    color: var(--black-light, #52596E);
+    font-size: 24px;    
+    font-weight: 500;
+    line-height: 158%;
+    margin-bottom: 64px;
+}
 
 `
 
-export const Backimg = styled.img`
+
+export const Frame = styled.div`
+display:flex;
+padding: 41px;
+border-radius: 8px;
+height: 168px;
+width: ${props=>props.blue ? "462px" : "378px"};
+background-color: ${props=>props.blue ? "#335DFF" : "#FD4455"};
+
+h1{
+    color: var(--white, #FCFCFF);
+    font-size: 40px;
+    font-weight: 800;
+    line-height: normal;
+    margin-top: -55px;
+}
+
+p{
+    color: var(--blue-light, #91A7FF);
+    font-size: 24px;    
+    font-weight: 700;
+    line-height: normal;
+   
+}
+
+`
+
+
+export const Container2 = styled.div`
+
+padding-left: ${props=>props.right ? "20px" : "0px"};
+
+
 
 `
